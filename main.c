c8d1707ef9db782249aef0b6d6f499cadb56faa5
nguyenson0610 <116062501+nguyenson0610@users.noreply.github.com>
2022-10-18 14:23:35 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include<stdint.h>
+
+uint8_t bien;
+
+
+int main(int argc, char const *argv[]){
+    uint8_t number = 7;
+    if(number >= 8){
+        printf("yeu");
+    }else if(number >= 5){
+        printf("tb");
+    }else if(number >= 3){
+        printf("kha");
+    }else if(number >= 1){
+        printf("gioi");
+    }
+    
+}
